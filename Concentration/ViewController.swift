3b78100eb0332c93239a01a8fbc96ddbca63e750
Unknown <Anthonylle@hotmail.com>
2018-07-11 00:35:54 -0600

    Initial files for the game
    I just created the game and set up a random button to make a test in the UI, will be deleted soon!

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -14,7 +14,27 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Mensaje", message: "Este es un mensaje", preferredStyle: UIAlertControllerStyle.alert)
+        
+        alert.addAction(UIAlertAction(title: "Yes", style: UIAlertActionStyle.default, handler: { (action) in
+            alert.dismiss(animated: true, completion: nil)
+            print("Yes")
+        }))
+        
+        alert.addAction(UIAlertAction(title: "No", style: UIAlertActionStyle.default, handler: { (action) in
+            alert.dismiss(animated: true, completion: nil)
+            print("No")
+        }))
+    
+        self.present(alert, animated: true, completion: nil)
+        
+        sender.setTitle("", for: UIControlState.normal)
+        sender.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
