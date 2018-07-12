ed74a9d1797fce0b981e9ecd6edee3772bf2b49d
Unknown <Anthonylle@hotmail.com>
2018-07-12 04:25:44 -0600

    Emojis on cards
    All cards have a random emoji on each one with a starting shuffle method for the static array of emojis, and then are mapped to a dictionary of Int(pos)->String(emoji) collection

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -13,20 +13,43 @@ class ViewController: UIViewController {
     @IBOutlet var cardButtons: [UIButton]!
     @IBOutlet weak var attemptsLabel: UILabel!
     var attempts = 0
+    var shuffled = [String]()
+    var emojiMap: [Int: String] = [:]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //print(emojis)
+        print(shuffleEmojis())
+        setCardsMapping()
+    }
+    
+    func shuffleEmojis() -> [String]{
+        var emojis = ["👻","🎃","🧟‍♀️","🕸","🦇","🌕","🔮","⚰️","🃏","🐸","👻","🎃","🧟‍♀️","🕸","🦇","🌕","🔮","⚰️","🃏","🐸","👻","🎃","🧟‍♀️","🕸","🦇","🌕","🔮","⚰️","🃏","🐸","👻","🎃","🧟‍♀️","🕸","🦇","🌕","🔮","⚰️","🃏","🐸"]
+        shuffled = []
+        for _ in 0..<emojis.count{
+            let rand = Int(arc4random_uniform(UInt32(emojis.count)))
+            shuffled.append(emojis[rand])
+            emojis.remove(at: rand)
+        }
+        return shuffled
     }
     
     func setCardsMapping(){
-        
+        for index in 0..<cardButtons.count{
+            emojiMap[index] = shuffled[index]
+            print("Inserted \(shuffled[index]) at pos \(index)")
+        }
+        print(emojiMap)
     }
     
     
     @IBAction func touchCard(_ sender: UIButton) {
         attempts += 1
         
+        let indexOfButton = cardButtons.index(of: sender)
+        print("Index of button is: \(indexOfButton!)")
         /*let alert = UIAlertController(title: "Mensaje", message: "Este es un mensaje", preferredStyle: UIAlertControllerStyle.alert)
         
         alert.addAction(UIAlertAction(title: "Yes", style: UIAlertActionStyle.default, handler: { (action) in
@@ -40,8 +63,8 @@ class ViewController: UIViewController {
         }))
     
         self.present(alert, animated: true, completion: nil) */
-        
-        sender.setTitle("🤯", for: UIControlState.normal)
+        print("Emoji map at \(indexOfButton!) is \(emojiMap[indexOfButton!]))")
+        sender.setTitle(emojiMap[indexOfButton!], for: UIControlState.normal)
         sender.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
         
         //attemptsLabel.setValue("Intentos fallidos: (\(attempts)/5)", forKey: )
